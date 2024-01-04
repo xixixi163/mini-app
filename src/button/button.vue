a71e8e2ac74f0cc468fbce069a68b4fb544188be
yiqian <1263678911@qq.com>
2024-01-04 23:28:02 +0800

    feat: button

--- src/button/button.vue
+++ src/button/button.vue
@@ -1,10 +1,22 @@
 <template>
-    <button>
-        button
+    <button class="ll-button" :class="buttonStyle">
+        <slot />
     </button>
 </template>
 <script lang="ts" setup>
+import "./style/index.less"
+import { computed } from 'vue';
+
 defineOptions({
     name: 'LlButton'
+})
+
+type buttonProps = {
+    type?: string;
+}
+const buttonProps = defineProps<buttonProps>()
+
+const buttonStyle = computed(() => {
+    return {[`ll-button--${buttonProps.type}`]: buttonProps.type}
 })
 </script>
