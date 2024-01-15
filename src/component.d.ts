fbf49901132ab4546e2d3d710d75cf358786f49e
yiqian <1263678911@qq.com>
2024-01-15 11:28:13 +0800

    feat: component declaration

--- src/component.d.ts
+++ src/component.d.ts
@@ -2,6 +2,7 @@ import * as components from "/index";
 declare module "@/vue/runtime-core" {
     export interface GlobalComponents {
         LlButton: typeof components.Button;
+        LlIcon: typeof components.Icon
     }
 }
 
